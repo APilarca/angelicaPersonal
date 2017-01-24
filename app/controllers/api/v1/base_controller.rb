8e2cd074c919ffd9d334dce3a41a6d104d6fac7e
APilarca <angelpill15@gmail.com>
2017-01-24 14:17:19 -0800

    create controllers

---
+++ app/controllers/api/v1/base_controller.rb
@@ -0,0 +1,3 @@
+class Api::V1::BaseController < AplicationController
+	respond_to :json
+end
---
+++ app/controllers/api/v1/comments_controller.rb
@@ -0,0 +1,25 @@
+class Api::V1::CommentsController < Api::V1::BaseController
+	def index
+		respond_with Comment.all
+	end
+
+	def create
+		respond_with :api, :v1, Comment.create(comment_params)
+	end
+
+	def destroy
+		respond_with Comment.destroy(params[:id])
+	end
+
+	def update
+		comment = Comment.find(params["id"])
+		comment.update_attributes(comment_params)
+		respond_with comment, json: comment
+	end
+
+	private
+
+	def comment_params
+		params.require(:comment).permit(:id, :name, :description)
+	end
+end
