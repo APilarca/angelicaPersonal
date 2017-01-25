bad4740690d5717a628433cb8909babbc5fefef8
APilarca <angelpill15@gmail.com>
2017-01-25 15:11:26 -0800

    add image to site

--- app/assets/javascripts/components/_header.js.jsx
+++
@@ -1,9 +0,0 @@
-var Header = React.createClass({
-	render() {
-		return(
-			<div>
-				<a href="https://www.linkedin.com/in/angelica-pilarca">#{asset_path()}</a>
-			</div>
-		)
-	}
-});
--- app/assets/javascripts/components/_projects.js.jsx
+++ app/assets/javascripts/components/_projects.js.jsx
@@ -2,6 +2,8 @@ var Projects = React.createClass({
 	render() {
 		return(
 			<h2>This is in projects</h2>
+			<a href="https://http://boot-scheduler.herokuapp.com/"><img className="project" src="<%= asset_path('AngelicaHeadshot.png') %>" /></a>
+			<a href="https://www.linkedin.com/in/angelica-pilarca"><img className="project" src="<%= asset_path('AngelicaHeadshot.png') %>" /></a>
 		)
 	}
 });
